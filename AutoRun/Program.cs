0698b3cfdbf26483c0b5da1c7cbb89c7eb571f09
dongnh <dongnh@gemmob.com>
2018-12-22 16:26:37 +0700

    hello

--- AutoRun/Program.cs
+++ AutoRun/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using AutoIt;
 namespace AutoRun
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			AutoItX.Run("notepad.exe", "");
+			AutoItX.WinWaitActive("Untitled");
+			AutoItX.Send("I'm in notepad");
+			AutoItX.Sleep(10000);
+			IntPtr winHandle = AutoItX.WinGetHandle("Untitled");
+			AutoItX.WinKill(winHandle);
 		}
 	}
 }
